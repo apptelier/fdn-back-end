ad6755d70563927aea5abb2dbb9f0b35c11c180e
Lesair Valmont <ixra.net@gmail.com>
2020-04-18 15:02:45 -0500

    [AB#300] fdn-utils project.

---
+++ fdn-utils/StringUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Ardalis.GuardClauses;
+
+namespace fdn_utils
+{
+    public class StringUtils
+    {
+        private static readonly Random Random = new Random();
+
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
+        public static string RandomString(int length)
+        {
+            Guard.Against.OutOfRange(length, nameof(length), 1, 255);
+
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+    }
+}
